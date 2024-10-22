b703a4e2caa4b9cb20e4fcd3858c6bed796536d4
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-10-22 18:43:44 -0300

    Criando FuncionarioUpdateView

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexTemplateView
 from .views import ListaFuncionarios
+from .views import FuncionarioUpdateView
 
 app_name = 'website'
 
 urlpatterns = [
-    path('', IndexView.as_view()),
-    path('lista', ListaFuncionarios.as_view())
+    path('', IndexTemplateView.as_view(), name='index'),
+    path('lista', ListaFuncionarios.as_view(), name='lista'),
+    path('atualiza/<id>', FuncionarioUpdateView.as_view(), name='atualiza')
 ]
--- website/views.py
+++ website/views.py
@@ -1,11 +1,14 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import ListView
 from django.views.generic import TemplateView
+from django.views.generic import UpdateView
 from .models import Funcionario
 
 # Create your views here.
 
-class IndexView(TemplateView):
+class IndexTemplateView(TemplateView):
     template_name = 'website\index.html'
 
 
@@ -13,3 +16,39 @@ class ListaFuncionarios(ListView):
     template_name = 'website/lista.html'
     model = Funcionario
     context_object_name = 'funcionarios'
+
+
+def criar_funcionario(request):
+    if request.method == 'POST':
+        form = FormularioDeCriacao(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('website/lista.html'))
+    else:
+        return render(request, 'website.form.html', {'form':form})
+
+
+class FuncionarioUpdateView(UpdateView):
+    template_name = 'website/atualiza.html'
+    model = Funcionario
+    fields = '__all__'
+
+    def get_object(self, queryset=None):
+        funcionario = None
+
+        # Os campos {pk} e {slug} estão presentes em self.kwargs
+        id = self.kwargs.get(self.pk_url_kwarg)
+        slug = self.kwargs.get(self.slug_url_kwarg)
+
+        if id is not None:
+            # Busca o funcionario apartir do id
+            funcionario = Funcionario.objects.filter(id=id).first()
+        elif slug is not None:
+            # Pega o campo slug do Model
+            campo_slug = self.get_slug_field()
+            # Busca o funcionario apartir do slug
+            funcionario = Funcionario.objects.filter(
+                **{campo_slug: slug}
+            ).first()
+            
+        return funcionario
