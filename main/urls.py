2b6d43c83d830dac09593a7811dbf1d3e3c775e3
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-10-22 15:40:49 -0300

    Atualizando arquivos

--- main/urls.py
+++ main/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Inclui as URLs do app ‘website’
+    path("", include("website.urls", namespace="website")),
+    # Interface administrativa
+    path("admin/", admin.site.urls),
 ]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'website/index.html')
+
+def lista_funcionarios(request):
+    return render(request, 'website/lista.html')
