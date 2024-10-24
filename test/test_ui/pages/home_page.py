e52a32c4b436c871a8f0836c947eefd6b9cb6852
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-10-24 18:03:40 -0300

    Plano de testes da Homepage

--- test/test_ui/pages/home_page.py
+++ test/test_ui/pages/home_page.py
@@ -6,9 +6,21 @@ class HomePage:
     def __init__(self, driver):
         self.driver = driver
         self.home = (By.XPATH, '//*[@class="nav-link" and contains(text(), "Página Inicial")]')
+        self.funcionario = (By.XPATH, '//*[@class="nav-link" and contains(text(), "Funcionários")]')
+        self.produto = (By.XPATH, '//*[@class="nav-link" and contains(text(), "Produtos")]')
+        self.venda = (By.XPATH, '//*[@class="nav-link" and contains(text(), "Vendas")]')
 
     def open(self):
         self.driver.get(self.URL)
 
-    def get_heading_text(self):
+    def get_heading_home_text(self):
         return self.driver.find_element(*self.home).text
+
+    def get_heading_funcionario_text(self):
+        return self.driver.find_element(*self.funcionario).text
+
+    def get_heading_produto_text(self):
+        return self.driver.find_element(*self.produto).text
+
+    def get_heading_venda_text(self):
+        return self.driver.find_element(*self.venda).text
--- test/test_ui/test_home.py
+++ test/test_ui/test_home.py
@@ -5,4 +5,19 @@ from .pages.home_page import HomePage
 def test_acesso_home_com_sucesso(webdriver):
     home_login = HomePage(webdriver)
     home_login.open()
-    assert home_login.get_heading_text() == 'Página Inicial'
+    assert home_login.get_heading_home_text() == 'Página Inicial'
+
+def test_verificar_home_se_aparece_funcionarios(webdriver):
+    home_login = HomePage(webdriver)
+    home_login.open()
+    assert home_login.get_heading_funcionario_text() == 'Funcionários'
+
+def test_verificar_home_se_aparece_produtos(webdriver):
+    home_login = HomePage(webdriver)
+    home_login.open()
+    assert home_login.get_heading_produto_text() == 'Produtos'
+
+def test_verificar_home_se_aparece_vendas(webdriver):
+    home_login = HomePage(webdriver)
+    home_login.open()
+    assert home_login.get_heading_venda_text() == 'Vendas'
