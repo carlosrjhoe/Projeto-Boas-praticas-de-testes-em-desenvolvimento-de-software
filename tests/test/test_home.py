4a1ecc862d020dc39539039280a8933123c35dd3
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-10-30 13:56:00 -0300

    STYLE: Style Update

--- tests/test/test_home.py
+++ tests/test/test_home.py
@@ -4,37 +4,38 @@ from time import sleep
 
 
 def test_home_access_success(webdriver):
-    home_login = HomePage(webdriver)
-    home_login.open()
-    assert home_login.get_heading_home_text() == "Página Inicial"
+    home = HomePage(webdriver)
+    home.open()
+    assert home.get_heading_home_text() == "Página Inicial"
 
 
 def test_verify_home_if_employees_appear(webdriver):
-    home_login = HomePage(webdriver)
-    home_login.open()
-    assert home_login.get_heading_funcionario_text() == "Funcionários"
+    home = HomePage(webdriver)
+    home.open()
+    assert home.get_heading_funcionario_text() == "Funcionários"
 
 
 def test_verify_home_if_products_appear(webdriver):
-    home_login = HomePage(webdriver)
-    home_login.open()
-    assert home_login.get_heading_produto_text() == "Produtos"
+    home = HomePage(webdriver)
+    home.open()
+    assert home.get_heading_produto_text() == "Produtos"
 
 
 def test_verify_home_if_sales_appear(webdriver):
-    home_login = HomePage(webdriver)
-    home_login.open()
-    assert home_login.get_heading_venda_text() == "Vendas"
+    home = HomePage(webdriver)
+    home.open()
+    assert home.get_heading_venda_text() == "Vendas"
 
 
 def test_click_the_employee_link_button(webdriver):
-    home_login = HomePage(webdriver)
-    home_login.open()
-    home_login.click_the_employee_link_button()
-    assert home_login.get_the_title_of_the_employees_page()
+    home = HomePage(webdriver)
+    home.open()
+    home.click_the_employee_link_button()
+    assert home.get_the_title_of_the_employees_page()
+
 
 def test_click_the_products_link_button(webdriver):
-    home_login = HomePage(webdriver)
-    home_login.open()
-    home_login.click_the_product_link_button()
-    assert home_login.get_the_title_of_the_products_page()
+    home = HomePage(webdriver)
+    home.open()
+    home.click_the_product_link_button()
+    assert home.get_the_title_of_the_products_page()
