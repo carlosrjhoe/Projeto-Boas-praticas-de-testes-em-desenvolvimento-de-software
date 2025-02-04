780a0fc08c627d35673dc74c94f59492b5ff6c2f
carlosrjhoe <carlosrjhoe@gmail.com>
2025-02-03 21:11:04 -0300

    REFACTOR: Reforando test_home

--- tests/test/test_home.py
+++ tests/test/test_home.py
@@ -17,34 +17,44 @@ def test_verify_home_if_employees_appear(webdriver):
     assert result == expected
 
 
-# def test_verify_home_if_products_appear(webdriver):
-#     home = HomePage(webdriver)
-#     home.open()
-#     assert home.get_heading_produto_text() == "Produtos"
-#
-#
-# def test_verify_home_if_sales_appear(webdriver):
-#     home = HomePage(webdriver)
-#     home.open()
-#     assert home.get_heading_venda_text() == "Vendas"
-#
-#
-# def test_click_the_employee_link_button(webdriver):
-#     home = HomePage(webdriver)
-#     home.open()
-#     home.click_the_employee_link_button()
-#     assert home.get_the_title_of_the_employees_page()
-#
-#
-# def test_click_the_products_link_button(webdriver):
-#     home = HomePage(webdriver)
-#     home.open()
-#     home.click_the_product_link_button()
-#     assert home.get_the_title_of_the_products_page()
-#
-#
-# def test_click_the_sales_link_button(webdriver):
-#     home = HomePage(webdriver)
-#     home.open()
-#     home.click_the_sales_link_button()
-#     assert home.get_the_title_of_the_sales_page()
+def test_verify_home_if_products_appear(webdriver):
+    home = HomePage(webdriver)
+    home.open()
+    result = home.get_heading_produto_text()
+    expected = "Produtos"
+    assert result == expected
+
+
+def test_verify_home_if_sales_appear(webdriver):
+    home = HomePage(webdriver)
+    home.open()
+    result = home.get_heading_venda_text()
+    expected = "Vendas"
+    assert result == expected
+
+
+def test_click_the_employee_link_button(webdriver):
+    home = HomePage(webdriver)
+    home.open()
+    home.click_the_employee_link_button()
+    result = home.get_the_title_of_the_employees_page()
+    expected = True
+    assert result == expected
+
+
+def test_click_the_products_link_button(webdriver):
+    home = HomePage(webdriver)
+    home.open()
+    home.click_the_product_link_button()
+    result = home.get_the_title_of_the_products_page()
+    expected = True
+    assert result == expected
+
+
+def test_click_the_sales_link_button(webdriver):
+    home = HomePage(webdriver)
+    home.open()
+    home.click_the_sales_link_button()
+    result = home.get_the_title_of_the_sales_page()
+    expected = True
+    assert result == expected
