095496429296d967b32df8e6ce12b204b858ab6a
Carlos Roberto <carlosrjhoe@gmail.com>
2025-02-06 22:11:37 -0300

    REFACTOR: retirando sleep co script de test

--- tests/test/test_funcionarios.py
+++ tests/test/test_funcionarios.py
@@ -4,17 +4,11 @@ from time import sleep
 
 def test_register_employee(webdriver):
     funcionario = FuncionarioPage(webdriver)
-    sleep(1)
     funcionario.open()
-    sleep(1)
     funcionario.click_the_employee_link_button()
-    sleep(1)
     funcionario.register_employee()
-    sleep(1)
     funcionario.fill_employee_form()
-    sleep(1)
     funcionario.send_registration()
-    sleep(1)
     result = funcionario.get_list_of_employees()
     expected = "Lista de Funcionários"
     assert result == expected
