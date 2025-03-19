a8dcb873689ae202cae502a40ec028d51e79d57e
Carlos Roberto <carlosrjhoe@gmail.com>
2025-03-19 20:39:14 -0300

    TEST: test_exclude_emplye PASSED

--- tests/test/test_funcionarios.py
+++ tests/test/test_funcionarios.py
@@ -1,5 +1,5 @@
 from tests.pages.funcionario.funcionario_page import FuncionarioPage
-
+from time import sleep
 
 def test_register_employee(webdriver):
     object_employee = FuncionarioPage(webdriver)
@@ -9,15 +9,25 @@ def test_register_employee(webdriver):
     object_employee.fill_employee_form()
     object_employee.send_registration()
     result = object_employee.get_list_of_employees()
-    expected = "Lista de Funcionários"
+    expected = 'Lista de Funcionários'
     assert result == expected
-
-def test_refresh_button_employee(webdriver):
+#
+# def test_refresh_button_employee(webdriver):
+#     object_employee = FuncionarioPage(webdriver)
+#     object_employee.open()
+#     object_employee.click_the_employee_link_button()
+#     object_employee.click_refresh_button()
+#     result = object_employee.get_input_name_employe()
+#     expected = object_employee.refresh_name_employe()
+#     object_employee.submit_change()
+#     assert result != expected
+#
+def test_exclude_emplye(webdriver):
     object_employee = FuncionarioPage(webdriver)
     object_employee.open()
     object_employee.click_the_employee_link_button()
-    object_employee.click_refresh_button()
-    result = object_employee.get_input_name_employe()
-    expected = object_employee.refresh_name_employe()
-    object_employee.submit_change()
-    assert result != expected
+    object_employee.exclude_emplye()
+    object_employee.submit_employee_exclusion()
+    result = object_employee.get_list_of_employees()
+    expected = 'Lista de Funcionários'
+    assert result == expected
