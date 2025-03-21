86ca5b2bd8eab48106a890d6b3bff49bff02eccd
Carlos Roberto <carlosrjhoe@gmail.com>
2025-03-21 19:10:27 -0300

    REFACTOR: test_refresh_button_employee

--- tests/test/test_funcionarios.py
+++ tests/test/test_funcionarios.py
@@ -11,17 +11,17 @@ def test_register_employee(webdriver):
     result = object_employee.get_list_of_employees()
     expected = 'Lista de Funcionários'
     assert result == expected
-#
-# def test_refresh_button_employee(webdriver):
-#     object_employee = FuncionarioPage(webdriver)
-#     object_employee.open()
-#     object_employee.click_the_employee_link_button()
-#     object_employee.click_refresh_button()
-#     result = object_employee.get_input_name_employe()
-#     expected = object_employee.refresh_name_employe()
-#     object_employee.submit_change()
-#     assert result != expected
-#
+
+def test_refresh_button_employee(webdriver):
+    object_employee = FuncionarioPage(webdriver)
+    object_employee.open()
+    object_employee.click_the_employee_link_button()
+    object_employee.click_refresh_button()
+    result = object_employee.get_input_name_employe()
+    expected = object_employee.refresh_name_employe()
+    object_employee.submit_change()
+    assert result != expected
+
 def test_exclude_emplye(webdriver):
     object_employee = FuncionarioPage(webdriver)
     object_employee.open()
