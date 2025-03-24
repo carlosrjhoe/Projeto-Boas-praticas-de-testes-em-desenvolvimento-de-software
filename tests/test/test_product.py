2b8a381abecbd07ac368c45c0a990f60b0a60d34
Carlos Roberto <carlosrjhoe@gmail.com>
2025-03-24 18:33:36 -0300

    TEST: test_register_employee PASSED

--- tests/test/test_product.py
+++ tests/test/test_product.py
@@ -1,16 +1,12 @@
 from tests.pages.produtos.produtos_pages import ProdutosPages
-from time import sleep
 
 def test_register_employee(webdriver):
     object_product = ProdutosPages(webdriver)
-    sleep(1)
     object_product.open()
-    sleep(1)
     object_product.click_the_product_link_button()
-    sleep(1)
     object_product.register_product_button()
-    sleep(1)
     object_product.register_product()
-    sleep(3)
     object_product.send_product_registration()
-    sleep(3)
+    result = object_product.get_list_of_product()
+    expected = 'Lista de Produtos'
+    assert result == expected
