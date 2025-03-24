4849367c53535bb1b1863225cc371e7abd315623
Carlos Roberto <carlosrjhoe@gmail.com>
2025-03-24 20:23:54 -0300

    TEST: test_exclude_product PASSED

--- tests/test/test_product.py
+++ tests/test/test_product.py
@@ -20,3 +20,13 @@ def test_refresh_button_product(webdriver):
     expected = object_product.refresh_name_product()
     object_product.submit_change()
     assert result != expected
+
+def test_exclude_product(webdriver):
+    object_product = ProdutosPages(webdriver)
+    object_product.open()
+    object_product.click_the_product_link_button()
+    object_product.exclude_product()
+    object_product.submit_product_exclusion()
+    result = object_product.get_list_of_product()
+    expected = 'Lista de Produtos'
+    assert result == expected
