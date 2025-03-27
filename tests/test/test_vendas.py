738a22ba69f7b0b6bb29e8789e4e21b7aa49a0c7
Carlos Roberto <carlosrjhoe@gmail.com>
2025-03-27 20:37:35 -0300

    TEST: test_refresh_button_product PASSED

--- tests/test/test_vendas.py
+++ tests/test/test_vendas.py
@@ -11,3 +11,15 @@ def test_register_sale(webdriver):
     result = object_sale.sale_registration_title()
     expected = True
     assert result == expected
+
+
+def test_refresh_button_product(webdriver):
+    object_sale = VendasPage(webdriver)
+    object_sale.open()
+    object_sale.click_the_sales_link_button()
+    object_sale.click_refresh_button()
+    object_sale.select_product()
+    object_sale.submit_change()
+    result = object_sale.get_list_of_product()
+    expected = 'Lista de Vendas Registradas'
+    assert result == expected
